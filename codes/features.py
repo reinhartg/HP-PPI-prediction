e187e23f9c5895bf0b5c53e71d2bbcefd7d8624c
ariston7 <reinhart.gunadi@yahoo.com>
2019-06-15 11:10:24 +0700

    Created feature extraction module

---
+++ codes/features.py
@@ -0,0 +1,8 @@
+# Functions to extract protein features for learning-based HP-PPI predictions
+
+def domain_features(protein):
+    '''Generate a feature vector of protein domain occurences in a protein.
+    
+    
+    
+    '''
