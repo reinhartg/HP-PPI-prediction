5d1470aa79505db7ba56e8c7e4a27e3b57901f41
ariston7 <reinhart.gunadi@yahoo.com>
2019-06-15 11:43:07 +0700

    Created function to extract domain features from a protein

--- codes/features.py
+++ codes/features.py
@@ -1,8 +1,31 @@
 # Functions to extract protein features for learning-based HP-PPI predictions
 
-def domain_features(protein):
+import numpy as np
+
+def domain_features(protein, domain_dict, domain_set):
     '''Generate a feature vector of protein domain occurences in a protein.
     
+    Parameters
+    ----------
+    protein : string
+        Protein identifier (must be contained in the keys of `domain_dict`)
     
+    domain_dict : dict
+        Mapping of protein identifiers to a list of its domains
     
+    domain_set : set, list, or list-like objects
+        Set of unique domains contained in datasets
+    
+    Return
+    ------
+    features : array, shape=[len(domain_set)]
+        Feature vector of domain occurences in `protein` (1 for presence and 0 for absence)
     '''
+    
+    domain_counts = []
+    for domain in domain_set:
+        c = domain_dict[protein].count(domain)
+        domain_counts.append(c)
+    
+    features = np.array(domain_counts)
+    return features
