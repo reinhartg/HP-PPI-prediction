92578a10011594503275bd796afbc38ea55ec7cb
ariston7 <reinhart.gunadi@yahoo.com>
2019-06-17 20:07:13 +0700

    Edited domain feature function to take a protein pair as an input instead of a single protein

--- codes/features.py
+++ codes/features.py
@@ -1,14 +1,13 @@
 # Functions to extract protein features for learning-based HP-PPI predictions
+from scipy.sparse import csr_matrix
 
-from scipy import sparse
-
-def domain_features(protein, domain_dict, domain_set):
+def domain_features(protein_pair, domain_dict, domain_set):
     '''Generate a feature vector of protein domain occurences in a protein.
     
     Parameters
     ----------
-    protein : string
-        Protein identifier (must be contained in the keys of `domain_dict`)
+    protein_pair : iterable
+        A pair of protein identifiers (must be contained in the keys of `domain_dict`)
     
     domain_dict : dict
         Mapping of protein identifiers to a list of its domains
@@ -18,16 +17,20 @@ def domain_features(protein, domain_dict, domain_set):
     
     Return
     ------
-    features : 1xlen(domain_set) sparse matrix
-        Feature vector of domain occurences in `protein` (1 for presence and 0 for absence) represented as a sparse matrix
+    features : list, shape=[len(pfam_set)]
+        Feature vector of domain occurences in `protein` (1 for presence and 0 for absence)
     '''
     
-    domain_profile = []
+    features = []
     for domain in domain_set:
-        if domain in domain_dict[protein]:
-            domain_profile.append(1)
+        
+        # Extract domains from pair
+        domains = domain_dict[protein_pair[0]] + domain_dict[protein_pair[1]]
+        
+        # Check existence of a domain in the pair
+        if domain in domains:
+            features.append(1)
         else:
-            domain_profile.append(0)
+            features.append(0)
     
-    features = sparse.csr_matrix(domain_profile)
-    return features
+    return csr_matrix(features)
