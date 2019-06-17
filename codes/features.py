e87dadf29d4a76ae2514f24e601c082dbba133bc
ariston7 <reinhart.gunadi@yahoo.com>
2019-06-18 00:20:32 +0700

    Resampling and reverted to extracting features per protein as opposed to per pair

--- codes/features.py
+++ codes/features.py
@@ -1,13 +1,13 @@
 # Functions to extract protein features for learning-based HP-PPI predictions
 from scipy.sparse import csr_matrix
 
-def domain_features(protein_pair, domain_dict, domain_set):
+def domain_features(protein, domain_dict, domain_set):
     '''Generate a feature vector of protein domain occurences in a protein.
     
     Parameters
     ----------
-    protein_pair : iterable
-        A pair of protein identifiers (must be contained in the keys of `domain_dict`)
+    protein : string
+        Protein identifier (must be contained in the keys of `domain_dict`)
     
     domain_dict : dict
         Mapping of protein identifiers to a list of its domains
@@ -17,20 +17,18 @@ def domain_features(protein_pair, domain_dict, domain_set):
     
     Return
     ------
-    features : list, shape=[len(pfam_set)]
+    features : sparse matrix, shape=[len(pfam_set)]
         Feature vector of domain occurences in `protein` (1 for presence and 0 for absence)
     '''
     
-    features = []
+    domain_profile = []
     for domain in domain_set:
-        
-        # Extract domains from pair
-        domains = domain_dict[protein_pair[0]] + domain_dict[protein_pair[1]]
-        
-        # Check existence of a domain in the pair
-        if domain in domains:
-            features.append(1)
+        # Check existence of a domain in the protein
+        if domain in domain_dict[protein]:
+            domain_profile.append(1)
         else:
-            features.append(0)
+            domain_profile.append(0)
     
-    return csr_matrix(features)
+    # Get features as a sparse matrix
+    features = csr_matrix(domain_profile)
+    return features
