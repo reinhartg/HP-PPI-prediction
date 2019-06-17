e7e37c6df79c4213fa3e83aad9eb10d2bbd0ba32
ariston7 <reinhart.gunadi@yahoo.com>
2019-06-17 18:06:49 +0700

    Modified domain feature extraction script to return csr_matrix instead of array

--- codes/features.py
+++ codes/features.py
@@ -1,6 +1,6 @@
 # Functions to extract protein features for learning-based HP-PPI predictions
 
-import numpy as np
+from scipy import sparse
 
 def domain_features(protein, domain_dict, domain_set):
     '''Generate a feature vector of protein domain occurences in a protein.
@@ -18,8 +18,8 @@ def domain_features(protein, domain_dict, domain_set):
     
     Return
     ------
-    features : array, shape=[len(domain_set)]
-        Feature vector of domain occurences in `protein` (1 for presence and 0 for absence)
+    features : 1xlen(domain_set) sparse matrix
+        Feature vector of domain occurences in `protein` (1 for presence and 0 for absence) represented as a sparse matrix
     '''
     
     domain_profile = []
@@ -29,5 +29,5 @@ def domain_features(protein, domain_dict, domain_set):
         else:
             domain_profile.append(0)
     
-    features = np.array(domain_profile)
+    features = sparse.csr_matrix(domain_profile)
     return features
